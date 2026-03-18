fe456dbf38f4387b5b6df3c44b2d64d507ff9a24
JuanD HernandezO <juand.hernandezo@autonoma.edu.co>
2026-03-18 18:49:23 -0500

    ListaSimple mod

--- ListaSimple.py
+++ ListaSimple.py
@@ -4,3 +4,16 @@ class ListaSimple:
     def __init__(self):
         self._cabeza = None
         self._tamanio = 0
+
+    @property
+    def cabeza(self):
+        return self._cabeza
+
+    @cabeza.setter
+    def cabeza(self, cabeza):
+        self._cabeza = cabeza
+
+    @property
+    def tamanio(self):
+        return self._tamanio
+
