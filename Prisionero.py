fc76343d4b9d3382ffb419dcef87b3a6fa94c37c
JuanD HernandezO <juand.hernandezo@autonoma.edu.co>
2026-03-18 18:45:41 -0500

    Prisionero modificado

--- Prisionero.py
+++ Prisionero.py
@@ -2,15 +2,19 @@ class Prisionero:
     def __init__(self, nombre, edad):
         self._nombre = nombre
         self._edad = edad
+
     @property
     def nombre(self):
         return self._nombre
+
     @property
     def edad(self):
         return self._edad
+
     @nombre.setter
     def nombre(self, nombre):
         self._nombre = nombre
+
     @edad.setter
     def edad(self, edad):
         self._edad = edad
