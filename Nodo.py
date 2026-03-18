609340f5add71cdcde2a2ae6969776e49aa927b2
JuanD HernandezO <juand.hernandezo@autonoma.edu.co>
2026-03-18 18:44:41 -0500

    Clase nodo modificada

--- Nodo.py
+++ Nodo.py
@@ -2,7 +2,14 @@ class Nodo:
     def __init__(self, prisionero):
         self._prisionero = prisionero
         self._siguiente = None
-        #self._anterior = None
+
+    @property
+    def prisionero(self):
+        return self._prisionero
+
+    @prisionero.setter
+    def prisionero(self, prisionero):
+        self._prisionero = prisionero
 
     @property
     def nombre(self):
@@ -11,3 +18,14 @@ class Nodo:
     @property
     def edad(self):
         return self._prisionero.edad
+
+    @property
+    def siguiente(self):
+        return self._siguiente
+
+    @siguiente.setter
+    def siguiente(self, siguiente):
+        self._siguiente = siguiente
+
+    def __str__(self):
+        return f"{self.nombre}({self.edad})"
