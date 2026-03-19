aa7c3b8f6c477707d460cead9fdf425e748753a2
JuanD HernandezO <juand.hernandezo@autonoma.edu.co>
2026-03-18 19:31:33 -0500

    Adelanto de segundo decreto

--- lista_simple.py
+++ lista_simple.py
@@ -42,6 +42,31 @@ class ListaSimple:
     def is_empty(self):
         return self._cabeza is None
 
+    def insertar(self, indice, prisionero):
+        nuevo_nodo = Nodo(prisionero)
+        if indice == 0:
+            nuevo_nodo.siguiente = self._cabeza
+            self._cabeza = nuevo_nodo
+            self._tamanio += 1
+            return
+        contador = 0
+        puntero = self._cabeza
+        while contador < (indice - 1) and puntero is not None:
+            puntero = puntero.siguiente
+            contador += 1
+        aux = puntero.siguiente
+        puntero.siguiente = nuevo_nodo
+        nuevo_nodo.siguiente = aux
+        self._tamanio += 1
+
+    def imprimir(self):
+        puntero = self._cabeza
+
+        while puntero is not None:
+            print(puntero)
+            puntero = puntero.siguiente
+
+
     # Primer detreto
     def ordenar(self):
         if self.is_empty():
@@ -53,7 +78,21 @@ class ListaSimple:
 
             while puntero.siguiente is not None:
                 if puntero.edad > puntero.siguiente.edad:
-                    puntero.prisionero, puntero.siguiente.prisionero = (puntero.siguiente.prisionero, puntero.prisionero)
+                    aux = puntero.prisionero
+                    puntero.prisionero = puntero.siguiente.prisionero
+                    puntero.siguiente.prisionero = aux
                     intercambiado = True
 
                 puntero = puntero.siguiente
+
+
+    # Acceder x indice
+    def _nodo_en_indice(self, indice):
+        anterior = None
+        actual = self._cabeza
+        for _ in range(indice):
+            anterior = actual
+            actual = actual.siguiente
+        return anterior, actual
+
+    # Segundo decreto - busq binaria y clemencia
