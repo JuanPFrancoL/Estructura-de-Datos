3ec0777050432d8cc4a798fe55c17c1a540fa250
JuanD HernandezO <juand.hernandezo@autonoma.edu.co>
2026-03-18 19:05:38 -0500

    Agregar y eliminar listos

--- lista_simple.py
+++ lista_simple.py
@@ -18,3 +18,23 @@ class ListaSimple:
         return self._tamanio
 
     # Metodos
+    def agregar_prisionero(self, prisionero):
+        nuevo_nodo = Nodo(prisionero)
+
+        if self._cabeza is None:
+            self._cabeza = nuevo_nodo
+        else:
+            puntero = self._cabeza
+            while puntero.siguiente is not None:
+                puntero = puntero.siguiente
+            puntero.siguiente = nuevo_nodo
+        self._tamanio += 1
+
+    def eliminar(self, nodo_anterior, nodo):
+        if nodo_anterior is None:
+            self._cabeza = nodo.siguiente
+        else:
+            nodo_anterior.siguiente = nodo.siguiente
+
+        nodo.siguiente = None
+        self._tamanio -= 1
